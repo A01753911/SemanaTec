d908d131a6f3950b6d4e98e3207f7ab7a15ed290
A01753911 <88682678+A01753911@users.noreply.github.com>
2023-10-25 11:05:12 -0600

    servidor basico

---
+++ index.js
@@ -0,0 +1,21 @@
+// Importar bibliotecas
+
+const express= require("express"); 
+const app = express()
+
+//middleware
+
+app.use(express.json());
+
+// Mala practica 
+
+app.get("/test", (request, response)=>{
+    console.log("Esto no se debe de hacer pero funciona");
+    response.send('<h1>Sevidor funcionado</h1>');
+})
+
+//levantar el server y escuchar peticiones
+
+app.listen(8080, () =>{
+    console.log("Sevidor escuchando")
+})
